266c4f803eb100118a582733f908b5b6ca5910dd
Travis Marks <travis@bmbx.net>
2021-10-13 20:52:51 -0600

    obs callbacks working

--- midibridge.py
+++ midibridge.py
@@ -6,17 +6,25 @@
 import json
 import mido
 from obswebsocket import obsws, events
+import time
 
 config = {}
 
 def on_obsevent(message):
     print(u"Message Received from OBS:{}".format(message))
 
-def on_obsswitch(message):
-    print(u"Scene changed to {}".format(message.getSceneName()))
-9
+def on_obspreviewswitch(message):
+    print(u"Preview Scene changed to {}".format(message.getSceneName()))
+
+def on_obstransition(message):
+    print(u"Transition from {} to {}".format(message.getFromScene(), message.getToScene()))
+
 def on_midi_msg(message):
     print(u"MidiMessage:{}".format(message))
+
+def on_obs_ignore(message):
+    #do nothing
+    return
 
 def midi_send(msg, device):
     print()
@@ -33,16 +41,21 @@ midin = mido.open_input(config['midi_input'], callback=on_midi_msg)
 midout = mido.open_output(config['midi_output'])
 
 #now we connect to OBS
+obs = obsws(config['server'], config['port'], config['password'])
+#obs.register(on_obsevent)
+obs.register(on_obspreviewswitch, events.PreviewSceneChanged)
+obs.register(on_obstransition, events.TransitionBegin)
+obs.register(on_obs_ignore, events.TransitionEnd)
+obs.register(on_obs_ignore, events.TransitionDurationChanged)
 
+obs.connect()
 
+try:
+    time.sleep(100)
+except KeyboardInterrupt:
+        pass
 
-
-
-
-
-
-
-
+obs.disconnect()
 midin.close()
 midout.close()
 
